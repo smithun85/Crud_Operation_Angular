f5424c5858c8c9c83e8e9e5f9c14cb4958702a9e
smithun85 <tech.smithun@gmail.com>
2023-07-15 21:57:20 +0530

    crud_Operation

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// FormsModule
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+//http for server;
+import { HttpClientModule } from '@angular/common/http';
+
+//dropdown
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+//modelModule:
+import { ModalModule } from 'ngx-bootstrap/modal';
+//pagination
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,   
+    ModalModule.forRoot(),
+    HttpClientModule,
+    BsDropdownModule.forRoot(),
+    PaginationModule.forRoot(),
+    FontAwesomeModule, 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
